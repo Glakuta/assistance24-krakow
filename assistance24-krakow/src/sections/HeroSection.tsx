0ce1abd8048e902efc83798ebf545e62c7086002
Glakuta <glakuta@gmail.com>
2023-04-26 21:42:53 +0200

    Update HeroSection

--- assistance24-krakow/src/sections/HeroSection.tsx
+++ assistance24-krakow/src/sections/HeroSection.tsx
@@ -1,10 +1,26 @@
+
+import CallButton from '@/components/CallButton'
 import React from 'react'
 
 type Props = {}
 
 const HeroSection = (props: Props) => {
   return (
-    <div>HeroSection</div>
+    <div className='p-9 flex-col h-96 w-full'>
+      <div className='flex-col items-center justify-center h-48'>
+      <CallButton />
+      <h1 className='pb-32 mb-6 flex items-center justify-center text-3xl font-bold'>Pomoc Drogowa Kraków</h1>
+      </div>
+      <div className='p-6 flex justify-around items-center w-auto bg-primary-yellow'>
+        <h3>AUTO-LAWETA</h3>
+        <h3>HOLOWANIE</h3>
+        <h3>POMOC</h3>
+        <h3>ASSISTANCE</h3>
+      </div>
+
+
+    </div>
+    
   )
 }
 

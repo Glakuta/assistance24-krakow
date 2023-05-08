f48d8d6fb2fcee1ff29b06aefcb060f8d5217c8a
Glakuta <glakuta@gmail.com>
2023-05-08 22:37:17 +0200

    Update navbar color

--- assistance24-krakow/src/components/NavBar.tsx
+++ assistance24-krakow/src/components/NavBar.tsx
@@ -9,10 +9,10 @@ const NavBar = (props: Props) => {
         <nav >
             <ul className='flex-row items-center hidden lg:flex mt-3 '>
                 <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>Strona główna</a></li>
-                <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Oferta</a></li>
-                <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Mobilny mechanik</a></li>
-                <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>O nas</a></li>
-                <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Kontakt</a></li>
+                <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>Oferta</a></li>
+                <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>Mobilny mechanik</a></li>
+                <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>O nas</a></li>
+                <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>Kontakt</a></li>
             </ul>
         </nav>
     </div>

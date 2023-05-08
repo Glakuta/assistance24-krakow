159e6eb5904cf2d9e7adb70d4d8ed0ec3b9c3a20
Glakuta <glakuta@gmail.com>
2023-05-08 22:27:31 +0200

    Change text color

--- assistance24-krakow/src/components/NavBar.tsx
+++ assistance24-krakow/src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = (props: Props) => {
         <div>Logo</div>
         <nav >
             <ul className='flex-row items-center hidden lg:flex mt-3 '>
-                <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Strona główna</a></li>
+                <li><a href='' className='pr-3 text-2xl font-semibold text-white hover:text-primary-yellow active:hover:text-primary-yellow'>Strona główna</a></li>
                 <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Oferta</a></li>
                 <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>Mobilny mechanik</a></li>
                 <li><a href='' className='pr-3 text-2xl font-semibold hover:text-primary-yellow active:hover:text-primary-yellow'>O nas</a></li>

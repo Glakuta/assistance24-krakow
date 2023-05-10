e7af3b978d5d3f2ed16633c843691d3c966f6d2e
Glakuta <glakuta@gmail.com>
2023-05-10 11:44:59 +0200

    Add phone icon

--- assistance24-krakow/src/components/CallButton.tsx
+++ assistance24-krakow/src/components/CallButton.tsx
@@ -1,12 +1,21 @@
+'use client'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 type Props = {}
 
 const CallButton = (props: Props) => {
   return (
-    <button className='flex items-center justify-center p-4 m-auto text-2xl font-bold text-white transition duration-300 ease-in-out delay-150 border-4 rounded shadow border-primary-yellow bg-blue hover:-translate-y-1 hover:scale-110'>
+    <div>
+      <a href="tel: 604 944 755">
+      <button className='flex items-center justify-center p-4 m-auto text-2xl font-bold text-white transition duration-300 ease-in-out delay-150 border-4 rounded shadow border-primary-yellow bg-blue hover:-translate-y-1 hover:scale-110'>
         Wezwij Pomoc!
+        <FontAwesomeIcon icon={faPhone} style={{color: "#ffffff", marginLeft: "10px"}} />
     </button>
+    </a>
+    </div>
+    
   )
 }
 

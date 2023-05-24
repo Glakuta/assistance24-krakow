b5c5f174de5f79b2a58d070e4216c846a6473b4c
Glakuta <92273750+Glakuta@users.noreply.github.com>
2023-05-24 20:04:08 +0200

    Update CallButton.tsx
    Move phone icon to left

--- assistance24-krakow/src/components/CallButton.tsx
+++ assistance24-krakow/src/components/CallButton.tsx
@@ -10,8 +10,8 @@ const CallButton = (props: Props) => {
     <div className='relative'>
       <a href="tel: 604 944 755">
       <button className='flex items-center justify-center p-4 m-auto text-2xl font-bold text-white transition duration-300 ease-in-out delay-150 border-4 rounded shadow border-primary-yellow bg-blue hover:-translate-y-1 hover:scale-110'>
-        Wezwij Pomoc!
-        <FontAwesomeIcon icon={faPhone} style={{color: "#ffffff", marginLeft: "10px"}} />
+        <FontAwesomeIcon icon={faPhone} style={{color: "#ffffff", marginRight: "10px"}} />
+        Wezwij Pomoc! 
     </button>
     </a>
     </div>

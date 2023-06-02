5f594c3af867abdef6ce2ba73de00791527c69da
Glakuta <glakuta@gmail.com>
2023-06-02 12:55:39 +0200

    Add domain to next config

--- assistance24-krakow/next.config.js
+++ assistance24-krakow/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["mechanik-chelm.s3.eu-central-1.amazonaws.com"],
+    domains: ["assistance24krakow.s3.eu-central-1.amazonaws.com"],
   },
 };
 
